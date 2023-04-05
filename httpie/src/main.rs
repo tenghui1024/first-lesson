760730e037d23df6a3145719796581d8a1b5a3aa
litenghui <644052732@qq.com>
2023-04-06 02:38:21 +0800

    added syntect

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -4,6 +4,12 @@ use colored::*;
 use mime::Mime;
 use reqwest::{header, Client, Response, Url};
 use std::{collections::HashMap, fmt::format, str::FromStr};
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -124,9 +130,8 @@ fn print_headers(resp: &Response) {
 // 打印服务器返回的HTTP body
 fn print_body(m: Option<Mime>, body: &String) {
     match m {
-        Some(v) if v == mime::APPLICATION_JSON => {
-            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
-        }
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
 
         // 其他 mime type 直接输出
         _ => println!("{}", body),
@@ -148,6 +153,20 @@ fn get_content_type(resp: &Response) -> Option<Mime> {
     resp.headers()
         .get(header::CONTENT_TYPE)
         .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+// 更强大的语法高亮
+fn print_syntect(s: &str, ext: &str) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(s) {
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
 }
 
 #[cfg(test)]
