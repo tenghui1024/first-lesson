ce0dcaab70bded4f00ec3f76b8bbf215700a1298
litenghui <644052732@qq.com>
2023-04-09 21:28:46 +0800

    更新测试用例

--- queryer/examples/covid.rs
+++ queryer/examples/covid.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use polars::prelude::*;
+use queryer::query;
 use std::io::Cursor;
 
 #[tokio::main]
@@ -8,6 +9,7 @@ async fn main() -> Result<()> {
 
     let url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv";
 
+    /*
     let data = reqwest::get(url).await?.text().await?;
 
     let df = CsvReader::new(Cursor::new(data))
@@ -25,6 +27,16 @@ async fn main() -> Result<()> {
             "new_deaths"
         ))
     );
+    */
+
+    // 使用 sql 从 URL 里获取数据
+    let sql = format!(
+        "SELECT location name, total_cases, new_cases, total_deaths, new_deaths \
+        FROM {} where new_death >= 500 ORDER BY new_cases DESC",
+        url
+    );
+    let df1 = query(sql).await?;
+    println!("{:?}", df1);
 
     Ok(())
 }
