dc47a3c739a2d82ab26ef1260951074c0c32e09e
litenghui <644052732@qq.com>
2023-04-09 21:16:24 +0800

    完善主流程

---
+++ queryer/src/loader.rs
@@ -0,0 +1,44 @@
+use crate::DataSet;
+use anyhow::Result;
+use polars::prelude::*;
+use std::io::Cursor;
+
+pub trait Load {
+    type Error;
+    fn load(self) -> Result<DataSet, Self::Error>;
+}
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum Loader {
+    Csv(CsvLoader),
+}
+
+#[derive(Default, Debug)]
+pub struct CsvLoader(pub(crate) String);
+
+impl Loader {
+    pub fn load(self) -> Result<DataSet> {
+        match self {
+            Loader::Csv(csv) => csv.load(),
+            // _ => Err(anyhow!("Only support CSV at the moment")),
+        }
+    }
+}
+
+pub fn detect_content(data: String) -> Loader {
+    // TODO: 内容检测 根据检测的内容返回Loader中对应格式的Loader
+    // 目前只支持Csv enum中只有CsvLoader, 直接返回
+    Loader::Csv(CsvLoader(data))
+}
+
+impl Load for CsvLoader {
+    type Error = anyhow::Error;
+
+    fn load(self) -> Result<DataSet, Self::Error> {
+        let df = CsvReader::new(Cursor::new(self.0))
+            .infer_schema(Some(16))
+            .finish()?;
+        Ok(DataSet(df))
+    }
+}
