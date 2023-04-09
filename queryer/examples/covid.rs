f60c5b9a67fd1da9bda0bc8c873d208d3b6e6482
litenghui <644052732@qq.com>
2023-04-09 15:49:03 +0800

    add example(covid) to polars

---
+++ queryer/examples/covid.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+use polars::prelude::*;
+use std::io::Cursor;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv";
+
+    let data = reqwest::get(url).await?.text().await?;
+
+    let df = CsvReader::new(Cursor::new(data))
+        .infer_schema(Some(16))
+        .finish()?;
+
+    let filtered = df.filter(&df["new_deaths"].gt(500))?;
+    println!(
+        "{:?}",
+        filtered.select((
+            "location",
+            "total_case",
+            "new_cases",
+            "total_deaths",
+            "new_deaths"
+        ))
+    );
+
+    Ok(())
+}
