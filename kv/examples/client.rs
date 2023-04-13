37d3f490dfce0c79dd62c34ae62f845f16bbfb73
litenghui <644052732@qq.com>
2023-04-14 03:45:41 +0800

    added kv example(client)

---
+++ kv/examples/client.rs
@@ -0,0 +1,28 @@
+use anyhow::Result;
+use async_prost::AsyncProstStream;
+use futures::prelude::*;
+use kv::{CommandRequest, CommandResponse};
+use tokio::net::TcpStream;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let addr = "127.0.0.1:9527";
+    // 链接服务器
+    let stream = TcpStream::connect(addr).await?;
+    // 使用AsyncProstStream 处理TCP Frame
+    let mut client =
+        AsyncProstStream::<_, CommandResponse, CommandRequest, _>::from(stream).for_sync();
+    // 生成一个HSET命令
+    let cmd = CommandRequest::new_hset("table1", "hello", "world".to_string().into());
+
+    // 发送HSET命令
+    client.send(cmd).await?;
+    if let Some(Ok(data)) = client.next().await {
+        info!("Got response {:?}", data);
+    }
+
+    Ok(())
+}
