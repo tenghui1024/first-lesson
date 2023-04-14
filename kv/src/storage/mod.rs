5fec16a9f36ad4e3a5524b2c607bbb4cde84145b
litenghui <644052732@qq.com>
2023-04-14 15:19:12 +0800

    添加memory(storage的一种实现)

--- kv/src/storage/mod.rs
+++ kv/src/storage/mod.rs
@@ -1,15 +1,16 @@
+mod memory;
 use crate::{KvError, Kvpair, Value};
+pub use memory::MemTable;
 
 /// 存储对象的抽象
 pub trait Storage {
     fn get(&self, table: &str, key: &str) -> Result<Option<Value>, KvError>;
-    fn set(&self, table: &str, key: &str, value: Value) -> Result<Option<Value>, KvError>;
-    fn contain(&self, table: &str, key: &str) -> Result<bool, KvError>;
+    fn set(&self, table: &str, key: String, value: Value) -> Result<Option<Value>, KvError>;
+    fn contains(&self, table: &str, key: &str) -> Result<bool, KvError>;
     fn del(&self, table: &str, key: &str) -> Result<Option<Value>, KvError>;
     fn get_all(&self, table: &str) -> Result<Vec<Kvpair>, KvError>;
     fn get_iter(&self, table: &str) -> Result<Box<dyn Iterator<Item = Kvpair>>, KvError>;
 }
-
 
 #[cfg(test)]
 mod tests {
