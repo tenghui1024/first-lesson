450996410720e364ffb8077daec1aeac35ebe4a7
litenghui <644052732@qq.com>
2023-04-18 17:24:30 +0800

    storage 模块支持sled数据库

--- kv/src/pb/mod.rs
+++ kv/src/pb/mod.rs
@@ -1,12 +1,15 @@
 pub mod abi;
 
+use std::convert::TryFrom;
+
 use abi::{command_request::RequestData, *};
+use bytes::Bytes;
 use http::StatusCode;
+use prost::Message;
 
 use crate::KvError;
 
 impl CommandRequest {
-    /// 创建 HSET 命令
     pub fn new_hget(table: impl Into<String>, key: impl Into<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hget(Hget {
@@ -16,7 +19,6 @@ impl CommandRequest {
         }
     }
 
-    /// 创建HGETALL命令
     pub fn new_hgetall(table: impl Into<String>) -> Self {
         Self {
             request_data: Some(RequestData::Hgetall(Hgetall {
@@ -25,7 +27,15 @@ impl CommandRequest {
         }
     }
 
-    /// 创建HSET命令
+    pub fn new_hmget(table: impl Into<String>, keys: Vec<String>) -> Self {
+        Self {
+            request_data: Some(RequestData::Hmget(Hmget {
+                table: table.into(),
+                keys,
+            })),
+        }
+    }
+
     pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
         Self {
             request_data: Some(RequestData::Hset(Hset {
@@ -34,10 +44,55 @@ impl CommandRequest {
             })),
         }
     }
+
+    pub fn new_hmset(table: impl Into<String>, pairs: Vec<Kvpair>) -> Self {
+        Self {
+            request_data: Some(RequestData::Hmset(Hmset {
+                table: table.into(),
+                pairs,
+            })),
+        }
+    }
+
+    pub fn new_hdel(table: impl Into<String>, key: impl Into<String>) -> Self {
+        Self {
+            request_data: Some(RequestData::Hdel(Hdel {
+                table: table.into(),
+                key: key.into(),
+            })),
+        }
+    }
+
+    pub fn new_hmdel(table: impl Into<String>, keys: Vec<String>) -> Self {
+        Self {
+            request_data: Some(RequestData::Hmdel(Hmdel {
+                table: table.into(),
+                keys,
+            })),
+        }
+    }
+
+    pub fn new_hexist(table: impl Into<String>, key: impl Into<String>) -> Self {
+        Self {
+            request_data: Some(RequestData::Hexist(Hexist {
+                table: table.into(),
+                key: key.into(),
+            })),
+        }
+    }
+
+    pub fn new_hmexist(table: impl Into<String>, keys: Vec<String>) -> Self {
+        Self {
+            request_data: Some(RequestData::Hmexist(Hmexist {
+                table: table.into(),
+                keys,
+            })),
+        }
+    }
 }
 
 impl Kvpair {
-    /// 创建一个新的kv pair
+    /// 创建一个新的 kv pair
     pub fn new(key: impl Into<String>, value: Value) -> Self {
         Self {
             key: key.into(),
@@ -46,7 +101,7 @@ impl Kvpair {
     }
 }
 
-/// 从String转换成Value
+/// 从 String 转换成 Value
 impl From<String> for Value {
     fn from(s: String) -> Self {
         Self {
@@ -64,7 +119,7 @@ impl From<&str> for Value {
     }
 }
 
-/// 从i64转换成Value
+/// 从 i64转换成 Value
 impl From<i64> for Value {
     fn from(i: i64) -> Self {
         Self {
@@ -73,7 +128,7 @@ impl From<i64> for Value {
     }
 }
 
-/// 从Value转换成CommandResponse
+/// 从 Value 转换成 CommandResponse
 impl From<Value> for CommandResponse {
     fn from(v: Value) -> Self {
         Self {
@@ -84,7 +139,7 @@ impl From<Value> for CommandResponse {
     }
 }
 
-/// 从Vec<Kvpair>转换成CommandResponse
+/// 从 Vec<Kvpair> 转换成 CommandResponse
 impl From<Vec<Kvpair>> for CommandResponse {
     fn from(v: Vec<Kvpair>) -> Self {
         Self {
@@ -95,7 +150,7 @@ impl From<Vec<Kvpair>> for CommandResponse {
     }
 }
 
-/// 从From<KvError>转换成CommandResponse
+/// 从 KvError 转换成 CommandResponse
 impl From<KvError> for CommandResponse {
     fn from(e: KvError) -> Self {
         let mut result = Self {
@@ -112,5 +167,93 @@ impl From<KvError> for CommandResponse {
         }
 
         result
+    }
+}
+
+impl From<Vec<Value>> for CommandResponse {
+    fn from(v: Vec<Value>) -> Self {
+        Self {
+            status: StatusCode::OK.as_u16() as _,
+            values: v,
+            ..Default::default()
+        }
+    }
+}
+
+impl From<bool> for Value {
+    fn from(b: bool) -> Self {
+        Self {
+            value: Some(value::Value::Bool(b)),
+        }
+    }
+}
+
+impl From<f64> for Value {
+    fn from(f: f64) -> Self {
+        Self {
+            value: Some(value::Value::Float(f)),
+        }
+    }
+}
+
+impl TryFrom<Value> for i64 {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Integer(i)) => Ok(i),
+            _ => Err(KvError::ConvertError(v, "Integer")),
+        }
+    }
+}
+
+impl TryFrom<Value> for f64 {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Float(f)) => Ok(f),
+            _ => Err(KvError::ConvertError(v, "Float")),
+        }
+    }
+}
+
+impl TryFrom<Value> for Bytes {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Binary(b)) => Ok(b),
+            _ => Err(KvError::ConvertError(v, "Binary")),
+        }
+    }
+}
+
+impl TryFrom<Value> for bool {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Bool(b)) => Ok(b),
+            _ => Err(KvError::ConvertError(v, "Boolean")),
+        }
+    }
+}
+
+impl TryFrom<Value> for Vec<u8> {
+    type Error = KvError;
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        let mut buf = Vec::with_capacity(v.encoded_len());
+        v.encode(&mut buf)?;
+        Ok(buf)
+    }
+}
+
+impl TryFrom<&[u8]> for Value {
+    type Error = KvError;
+
+    fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
+        let msg = Value::decode(data)?;
+        Ok(msg)
     }
 }
--- kv/src/storage/mod.rs
+++ kv/src/storage/mod.rs
@@ -1,4 +1,5 @@
 mod memory;
+mod sleddb;
 use crate::{KvError, Kvpair, Value};
 pub use memory::MemTable;
 
@@ -7,7 +8,12 @@ pub trait Storage {
     /// 从一个HashTable里获取一个key 的 value
     fn get(&self, table: &str, key: &str) -> Result<Option<Value>, KvError>;
     /// 从一个HashTable里设置一个key的value
-    fn set(&self, table: &str, key: String, value: Value) -> Result<Option<Value>, KvError>;
+    fn set(
+        &self,
+        table: &str,
+        key: impl Into<String>,
+        value: impl Into<Value>,
+    ) -> Result<Option<Value>, KvError>;
     /// 查看HashTable中是否有key
     fn contains(&self, table: &str, key: &str) -> Result<bool, KvError>;
     /// 从HashTable中删除一个key
@@ -64,10 +70,10 @@ mod tests {
 
     fn test_basi_interface(store: impl Storage) {
         // 第一次 set 会创建 table，插入 key 并返回 None（之前没值）
-        let v = store.set("t1", "hello".into(), "world".into());
+        let v = store.set("t1", "hello", "world");
         assert!(v.unwrap().is_none());
         // 再次 set 同样的 key 会更新，并返回之前的值
-        let v1 = store.set("t1", "hello".into(), "world1".into());
+        let v1 = store.set("t1", "hello", "world1");
         assert_eq!(v1, Ok(Some("world".into())));
 
         // get 存在的 key 会得到最新的值
@@ -93,8 +99,8 @@ mod tests {
     }
 
     fn test_get_all(store: impl Storage) {
-        store.set("t2", "k1".into(), "v1".into()).unwrap();
-        store.set("t2", "k2".into(), "v2".into()).unwrap();
+        store.set("t2", "k1", "v1").unwrap();
+        store.set("t2", "k2", "v2").unwrap();
         let mut data = store.get_all("t2").unwrap();
         data.sort_by(|a, b| a.partial_cmp(b).unwrap());
         assert_eq!(
@@ -107,8 +113,8 @@ mod tests {
     }
 
     fn test_get_iter(store: impl Storage) {
-        store.set("t2", "k1".into(), "v1".into()).unwrap();
-        store.set("t2", "k2".into(), "v2".into()).unwrap();
+        store.set("t2", "k1", "v1").unwrap();
+        store.set("t2", "k2", "v2").unwrap();
         let mut data: Vec<_> = store.get_iter("t2").unwrap().collect();
         data.sort_by(|a, b| a.partial_cmp(b).unwrap());
         assert_eq!(
