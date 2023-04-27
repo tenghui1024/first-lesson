5a564a2a4a8d5d65b395920e3fe43df6b9d174a8
litenghui <644052732@qq.com>
2023-04-28 03:48:00 +0800

    使用syn/quote重写Builder派生宏

--- macros/src/builder.rs
+++ macros/src/builder.rs
@@ -1,1 +1,159 @@
+//! 1. 打印DeriveInput结构
+//! 2. 定义自己用于处理derive宏的数据结构
+//! 3. 把DeriveInput转换成自己的数据结构
+//! 4. 使用quote生成代码
+//! 5. 完整实现
+use proc_macro2::{Ident, TokenStream};
+use quote::quote;
+use syn::{
+    Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, GenericArgument, Path, Type,
+    TypePath,
+};
 
+/// 描述字段所有信息
+struct Fd {
+    name: Ident,
+    ty: Type,
+    optional: bool,
+}
+
+/// 描述 struct 的所有信息
+pub struct BuilderContext {
+    name: Ident,
+    fields: Vec<Fd>,
+}
+
+/// 把一个Field转换成Fd
+impl From<Field> for Fd {
+    fn from(f: Field) -> Self {
+        let (optional, ty) = get_option_inner(&f.ty);
+        Self {
+            name: f.ident.unwrap(),
+            optional,
+            ty: ty.to_owned(),
+        }
+    }
+}
+
+/// 把DeriveInput转换成BuilderContext
+impl From<DeriveInput> for BuilderContext {
+    fn from(input: DeriveInput) -> Self {
+        let name = input.ident;
+
+        let fields = if let Data::Struct(DataStruct {
+            fields: Fields::Named(FieldsNamed { named, .. }),
+            ..
+        }) = input.data
+        {
+            named
+        } else {
+            panic!("Unsupported data type");
+        };
+
+        let fds = fields.into_iter().map(Fd::from).collect();
+        Self { name, fields: fds }
+    }
+}
+
+fn get_option_inner(ty: &Type) -> (bool, &Type) {
+    // 首先模式匹配出segments
+    if let Type::Path(TypePath {
+        path: Path { segments, .. },
+        ..
+    }) = ty
+    {
+        if let Some(v) = segments.iter().next() {
+            if v.ident == "Option" {
+                let t = match &v.arguments {
+                    syn::PathArguments::AngleBracketed(a) => match a.args.iter().next() {
+                        Some(GenericArgument::Type(t)) => t,
+                        _ => panic!("Not sure what to do with other GenericArgument"),
+                    },
+                    _ => panic!("Not sure what to do with other PathArguments"),
+                };
+                return (true, t);
+            }
+        }
+    }
+
+    return (false, ty);
+}
+
+impl BuilderContext {
+    pub fn render(&self) -> TokenStream {
+        let name = &self.name;
+        let builder_name = Ident::new(&format!("{}Builder", name), name.span());
+
+        let optionized_fields = self.gen_optionized_fields();
+        let methods = self.gen_methods();
+        let assigns = self.gen_assigns();
+
+        quote!(
+            /// Builder结构
+            #[derive(Debug, Default)]
+            struct #builder_name {
+                #(#optionized_fields,)*
+            }
+
+            /// Builder结构体每个字短赋值的方法，以及build()方法
+            impl #builder_name {
+                #(#methods)*
+
+                pub fn build(mut self) -> Result<#name, &'static str> {
+                    Ok(#name {
+                        #(#assigns,)*
+                    })
+                }
+            }
+
+            /// 为使用Builder的原结构提供builder()方法, 生成Builder结构
+            impl #name {
+                fn builder() -> #builder_name {
+                    Default::default()
+                }
+            }
+        )
+    }
+
+    fn gen_optionized_fields(&self) -> Vec<TokenStream> {
+        self.fields
+            .iter()
+            .map(|Fd { name, ty, .. }| {
+                quote! {
+                    #name: std::option::Option<#ty>
+                }
+            })
+            .collect()
+    }
+
+    fn gen_methods(&self) -> Vec<TokenStream> {
+        self.fields
+            .iter()
+            .map(|Fd { name, ty, .. }| {
+                quote! {
+                   pub fn #name(mut self, v: impl Into<#ty>) -> Self {
+                        self.#name = Some(v.into());
+                        self
+                    }
+                }
+            })
+            .collect()
+    }
+
+    fn gen_assigns(&self) -> Vec<TokenStream> {
+        self.fields
+            .iter()
+            .map(|Fd { name, optional, .. }| {
+                if *optional {
+                    return quote! {
+                        #name: self.#name.take()
+                    };
+                }
+
+                quote! {
+                    #name: self.#name.take().ok_or(concat!(stringify(#name)))
+                }
+            })
+            .collect()
+    }
+}
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,6 +3,7 @@ mod raw_builder;
 
 use proc_macro::TokenStream;
 use raw_builder::BuilderContext;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro]
 pub fn query(input: TokenStream) -> TokenStream {
@@ -17,4 +18,13 @@ pub fn derive_raw_builder(input: TokenStream) -> TokenStream {
     //println!("{:#?}", input);
     //TokenStream::default()
     BuilderContext::render(input).unwrap().parse().unwrap()
+}
+
+#[proc_macro_derive(Builder)]
+pub fn derive_builder(input: TokenStream) -> TokenStream {
+    // 解析输入的tokens到一个语法树
+    let input = parse_macro_input!(input as DeriveInput);
+    // println!("{:#?}", input);
+    // TokenStream::default()
+    builder::BuilderContext::from(input).render().into()
 }
